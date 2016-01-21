63fe273d50624cecad15dece31521a81d688baaa
James B. Pollack <jamesbradenpollack@gmail.com>
2016-01-20 17:24:54 -0800

    make ungrabbable

--- cellscience/importCellScience.js
+++ cellscience/importCellScience.js
@@ -3,7 +3,56 @@ var cellLayout;
 var path = Script.resolvePath('importCellScience.js')
 baseLocation = path.split('importCellScience')[0];
 print('baseLocation IS :::: ' + path)
+setEntityUserData = function(id, data) {
+    var json = JSON.stringify(data)
+    Entities.editEntity(id, { userData: json });
+}
 
+// FIXME do non-destructive modification of the existing user data
+getEntityUserData = function(id) {
+    var results = null;
+    var properties = Entities.getEntityProperties(id, "userData");
+    if (properties.userData) {
+        try {
+            results = JSON.parse(properties.userData);
+        } catch(err) {
+            logDebug(err);
+            logDebug(properties.userData);
+        }
+    }
+    return results ? results : {};
+}
+
+
+// Non-destructively modify the user data of an entity.
+setEntityCustomData = function(customKey, id, data) {
+    var userData = getEntityUserData(id);
+    if (data == null) {
+        delete userData[customKey];
+    } else {
+        userData[customKey] = data;
+    }
+    setEntityUserData(id, userData);
+}
+
+getEntityCustomData = function(customKey, id, defaultValue) {
+    var userData = getEntityUserData(id);
+    if (undefined != userData[customKey]) {
+        return userData[customKey];
+    } else {
+        return defaultValue;
+    }
+}
+
+function makeUngrabbable(entityID){
+print('making ungrabbable',entityID)
+setEntityCustomData('grabbableKey', entityID, {grabbable:false});
+}
+
+   Entities.addingEntity.connect(function(entityId) {
+        makeUngrabbable(entityId);
+    });
+   
 assignVariables();
 
 var locations = {
@@ -714,7 +763,7 @@ function deleteAllInRadius(position, radius) {
     for (var i = 0; i < arrayFound.length; i++) {
 
         Entities.deleteEntity(arrayFound[i]);
-        Entities.deletingEntity(arrayFound[i]);
+        // Entities.deletingEntity(arrayFound[i]);
     }
     //  print("deleted " + arrayFound.length + " entities");
 }
